b6face7b94e2ecdc259f256598220bdd4650a443
chrlskrt <repuestocharlene@gmail.com>
2024-03-16 21:44:44 +0800

    revised [login.php]

--- login.php
+++ login.php
@@ -24,6 +24,46 @@
   </div>
 </div>
 
+<div class="modal fade" tabindex="-1" role="dialog" id="unameErrorModal">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">OMO!!</h5>
+        <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>[username] does not exists in our records. Try again or sign up.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <a href="register.php" class="btn btn-outline-success">SIGN UP</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" tabindex="-1" role="dialog" id="passErrorModal">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">OMO!!</h5>
+        <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Incorrect [password]. Try again or sign up.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <a href="register.php" class="btn btn-outline-success">SIGN UP</a>
+      </div>
+    </div>
+  </div>
+</div>
+
 <section class="CreateNLog">
     <div>
         <p class="label">Log In</p>
@@ -70,18 +110,24 @@
 		$row = mysqli_fetch_array($result);
 		
         // var_dump($row);
-		if($count == 0 && password_verify($pwd, $row[4])) {
+		if($count == 0) {
+			echo "<script language = 'javascript'>
+						$(function(){
+                            $('#unameErrorModal').modal('show');
+                        })
+				  </script>";
+		 } else if (!password_verify($pwd, $row[4])){
+            echo "<script language = 'javascript'>
+						$(function(){
+                            $('#username').val('".$uname."');
+                            $('#passErrorModal').modal('show');
+                        })
+				  </script>";
+		} else {
             // if naa nay concept na log-out log-out or mag add ta
             // $logInCookie = urlencode(base64_encode(serialize($row)));
             // setcookie('user', $logInCookie, time() + (86400 * 30), "/");
 			header("Location: index.php");
-			
-		}else{
-            echo "<script language = 'javascript'>
-						$(function(){
-                            $('#errorModal').modal('show');
-                        })
-				  </script>";
-		}
+        }
 	}
 ?>
