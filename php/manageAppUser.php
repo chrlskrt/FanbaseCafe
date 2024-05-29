65ca0add562122dea6845116d1c583abedc00e1a
chrlskrt <repuestocharlene@gmail.com>
2024-05-30 00:26:35 +0800

    tblsysadmin deleted

--- php/manageAppUser.php
+++ php/manageAppUser.php
@@ -10,16 +10,16 @@
         $sqlPromoteUser = "UPDATE tbluseraccount SET isSysAdmin = 1 WHERE account_id = {$account_id}";
         $resultPromoteUser = mysqli_query($connection, $sqlPromoteUser);
 
-        $checkUserSysAdminStat = "SELECT sysAdmin_id FROM tbluseraccount_sysadmin WHERE account_id = {$account_id}";
-        $result = mysqli_fetch_assoc(mysqli_query($connection, $checkUserSysAdminStat));
+        // $checkUserSysAdminStat = "SELECT sysAdmin_id FROM tbluseraccount_sysadmin WHERE account_id = {$account_id}";
+        // $result = mysqli_fetch_assoc(mysqli_query($connection, $checkUserSysAdminStat));
 
-        if (!$result){
-            $sqlInsert = "INSERT INTO tbluseraccount_sysadmin (account_id, date_appointed) VALUES ('$account_id', NOW())";
-            $res = mysqli_query($connection, $sqlInsert);
-        } else {
-            $sqlUpdate = "UPDATE tbluseraccount_sysadmin SET isDemoted = 0, date_appointed = NOW() WHERE account_id = {$account_id}";
-            $res = mysqli_query($connection, $sqlUpdate);
-        }
+        // if (!$result){
+        //     $sqlInsert = "INSERT INTO tbluseraccount_sysadmin (account_id, date_appointed) VALUES ('$account_id', NOW())";
+        //     $res = mysqli_query($connection, $sqlInsert);
+        // } else {
+        //     $sqlUpdate = "UPDATE tbluseraccount_sysadmin SET isDemoted = 0, date_appointed = NOW() WHERE account_id = {$account_id}";
+        //     $res = mysqli_query($connection, $sqlUpdate);
+        // }
 
         if ($resultPromoteUser){
             header('Location: ../manageApp.php?promoteUser');
@@ -34,8 +34,8 @@
         $sqlDemoteUser = "UPDATE tbluseraccount SET isSysAdmin = 0 WHERE account_id = {$account_id}";
         $resultDemoteUser = mysqli_query($connection, $sqlDemoteUser);
 
-        $sqlDemoteUser = "UPDATE tbluseraccount_sysAdmin SET isDemoted = 1 WHERE account_id = {$account_id}";
-        $resultDemoteUser = mysqli_query($connection, $sqlDemoteUser);
+        // $sqlDemoteUser = "UPDATE tbluseraccount_sysAdmin SET isDemoted = 1 WHERE account_id = {$account_id}";
+        // $resultDemoteUser = mysqli_query($connection, $sqlDemoteUser);
 
         if ($resultDemoteUser){
             header('Location: ../manageApp.php?demoteUser');
